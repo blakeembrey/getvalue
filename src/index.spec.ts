e9e2fc65cde64923d51aa40584c69332b5a81afa
Blake Embrey <hello@blakeembrey.com>
2017-06-27 19:50:21 -0400

    Move to `@types`, use generic defaults

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,8 +2,10 @@ import test = require('blue-tape')
 import { get } from './index'
 
 test('getvalue', t => {
+  const test: { missing?: { prop?: number } } = {}
+
   t.equal(get({ a: { b: { c: 10 } } }, ['a', 'b', 'c']), 10)
-  t.equal(get({ nothere: true }, ['here']), undefined)
-  t.equal(get({}, ['hey'], true), true)
+  t.equal(get(test, ['missing', 'prop']), undefined)
+  t.equal(get(test, ['missing', 'prop'], 10), 10)
   t.end()
 })
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ const _hasOwnProperty = Object.prototype.hasOwnProperty
 
 export type Path = Array<string | number | symbol>
 
-export function get <T> (obj: any, path: Path, defaultValue?: T): T {
+export function get <T = any> (obj: any, path: Path, defaultValue: T): T
+export function get <T = any> (obj: any, path: Path, defaultValue?: T): T | undefined
+export function get <T = any> (obj: any, path: Path, defaultValue?: T): T | undefined {
   let res = obj
 
   for (const key of path) {
