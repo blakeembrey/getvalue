81fb2110b9cfe05d1ae2739bb3f4a54c4411c22a
ycjcl868 <45808948@qq.com>
2017-07-21 13:12:51 +0800

    test: go together

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,14 +7,8 @@ test('getvalue', t => {
   t.equal(get({ a: { b: { c: 10 } } }, ['a', 'b', 'c']), 10)
   t.equal(get(test, ['missing', 'prop']), undefined)
   t.equal(get(test, ['missing', 'prop'], 10), 10)
-  t.end()
-})
-
-test('getArray', t => {
-  const test: { missing?: { prop?: number } } = {}
 
   t.equal(get({ a: { b: { c: [ { d: 1 } ] } } }, ['a', 'b', 'c', '0', 'd']), 1)
   t.equal(get({ a: { b: { c: [ { d: 1 } ] } } }, ['a', 'b', 'c', '2', 'd']), undefined)
-  t.equal(get(test, ['missing', 'prop']), undefined)
   t.end()
 })
