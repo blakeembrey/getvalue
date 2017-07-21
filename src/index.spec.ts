5eeb8cea71fea4025b0824ac87fdd46af47bf6c1
信鑫-King <45808948@qq.com>
2017-07-22 04:18:11 +0800

    Add tests using array indices (#7)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,5 +7,8 @@ test('getvalue', t => {
   t.equal(get({ a: { b: { c: 10 } } }, ['a', 'b', 'c']), 10)
   t.equal(get(test, ['missing', 'prop']), undefined)
   t.equal(get(test, ['missing', 'prop'], 10), 10)
+
+  t.equal(get({ a: { b: { c: [ { d: 1 } ] } } }, ['a', 'b', 'c', '0', 'd']), 1)
+  t.equal(get({ a: { b: { c: [ { d: 1 } ] } } }, ['a', 'b', 'c', '2', 'd']), undefined)
   t.end()
 })
